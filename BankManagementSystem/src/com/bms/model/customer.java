ea40f934042367c15421919cfdd78792a8162abb
Mada Upendra <upendravarma1234@gmail.com>
2024-12-19 23:24:53 -0600

    Customer.java class has been created

---
+++ BankManagementSystem/src/com/bms/model/customer.java
@@ -0,0 +1,48 @@
+package com.bms.model;
+
+public class customer {
+
+    private String customerName;
+    private int customerId;
+    private String customerAddress;
+
+
+    //constructor
+    public customer(String customerName, int customerId, String customerAddress) {
+        this.customerName = customerName;
+        this.customerId = customerId;
+        this.customerAddress = customerAddress;
+    }
+
+
+    //getter setter to ensure encapsulation
+    //why we are setting getters and setters as public
+
+    public  String getcustomerName(){
+        return customerName;
+    }
+    public void setCustomerName(String customerName) {
+        this.customerName=customerName;
+    }
+
+    public int getcustomerId(){
+        return customerId;
+    }
+    public void setCustomerId(int customerId){
+        this.customerId=customerId;
+    }
+    public String getcustomerAddress(){
+        return customerAddress;
+    }
+    public void setcustomerAddress(String customerAddress){
+        this.customerAddress=customerAddress;
+    }
+
+    //to string method
+
+    public String tostring(){
+        return "customerName: " +customerName + ", customerId: " + customerId + ", customerAddress: " + customerAddress;
+
+    }
+
+}
